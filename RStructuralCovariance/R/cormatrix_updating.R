a5b689cd5e9a2810037bd3fa4ae0e8ecd2d9cdc6
yohanyee <yohan.yee@gmail.com>
2017-09-02 03:28:52 -0400

    Bayesian updating of the covariance matrix

---
+++ RStructuralCovariance/R/cormatrix_updating.R
@@ -0,0 +1,25 @@
+
+update_covariance_matrix <- function(prior_covmtx, prior_n, update_covmtx, update_n, confidence_intervals=NULL, ci_sample_size=10000) {
+  S <- prior_n*prior_covmtx
+  v <- dim(S)[1]
+  
+  out <- list()
+  out[["posterior_mean"]] <- (update_covmtx*update_n + S)/(update_n - 1)
+  out[["posterior_mode"]] <- (update_covmtx*update_n + S)/(2*v + update_n + 1)
+  
+  if (is.numeric(confidence_intervals)) {
+    posterior_samples <- array(dim = c(dim(S)[1], dim(S)[2], ci_sample_size), dimnames = list(rownames(S), colnames(S), paste("sample", 1:ci_sample_size, sep="_")))
+    for (i in 1:ci_sample_size) {
+      posterior_samples[,,i] <- LaplacesDemon::rinvwishart(nu = (update_n + v), S = (update_covmtx*update_n + S))
+    }
+    
+    out[["posterior_samples"]] <- posterior_samples
+    out[["posterior_confidence_intervals"]] <- list()
+    for (cint in confidence_intervals) {
+      out[["posterior_confidence_intervals"]][[paste(100*cint, "%", sep="")]] <- apply(posterior_samples, c(1,2), "quantile", cint)
+    }
+  }
+  
+  return(out)
+}
+
