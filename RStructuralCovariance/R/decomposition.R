49b0a96a9695eac44e8c25f01713fccacc7ebc2c
Ben Darwin <bcdarwin@gmail.com>
2018-07-03 16:06:12 -0400

    comment out code in decomposition.R since 'X' is unbound

--- RStructuralCovariance/R/decomposition.R
+++ RStructuralCovariance/R/decomposition.R
@@ -1,6 +1,6 @@
-o <- hclust(dist(cov2cor(X)), method="average")$order
+#o <- hclust(dist(cov2cor(X)), method="average")$order
 
-corrplot(cov2cor(X)[o,o], method="color", tl.col="black", tl.cex=0.2, col=col2rev(100))
+#corrplot(cov2cor(X)[o,o], method="color", tl.col="black", tl.cex=0.2, col=col2rev(100))
 
 
 pca_reconstruct <- function(X, n=5) {
@@ -10,16 +10,16 @@ pca_reconstruct <- function(X, n=5) {
   return(as.matrix(Matrix::nearPD(X_recon)$mat))
 }
 
-X_recon <- cov2cor(pca_reconstruct(X, n=20))
-X_recon[X_recon < -1] <- -1
-X_recon[X_recon > 1] <- 1
+#X_recon <- cov2cor(pca_reconstruct(X, n=20))
+#X_recon[X_recon < -1] <- -1
+#X_recon[X_recon > 1] <- 1
 
-corrplot(cov2cor(X)[o,o], method="color", tl.col="black", tl.cex=0.2, col=col2rev(100))
-corrplot(cov2cor(X_recon)[o,o], method="color", tl.col="black", tl.cex=0.2, col=col2rev(100))
-corrplot(X_recon[o,o], method="color", tl.col="black", tl.cex=0.2, col=col2rev(100))
+#corrplot(cov2cor(X)[o,o], method="color", tl.col="black", tl.cex=0.2, col=col2rev(100))
+#corrplot(cov2cor(X_recon)[o,o], method="color", tl.col="black", tl.cex=0.2, col=col2rev(100))
+#corrplot(X_recon[o,o], method="color", tl.col="black", tl.cex=0.2, col=col2rev(100))
 
 
-mvrnorm(n=10, mu = colMeans(X), Sigma=lowrank_cov$mat)
+#mvrnorm(n=10, mu = colMeans(X), Sigma=lowrank_cov$mat)
 
-X_cor <- X_recon/max(abs(X_recon))
-pca_reconstruct(X, n=20)
+#X_cor <- X_recon/max(abs(X_recon))
+#pca_reconstruct(X, n=20)
